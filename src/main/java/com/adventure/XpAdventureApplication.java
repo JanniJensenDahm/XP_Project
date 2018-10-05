7cee4d8af0fddedec77610ef48810de7d02ba2f6
a5vTech <33541120+a5vTech@users.noreply.github.com>
2018-10-05 09:27:41 +0200

    INITIAL PROJECT STRUCTURE

---
+++ src/main/java/com/adventure/XpAdventureApplication.java
@@ -0,0 +1,12 @@
+package com.adventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpAdventureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XpAdventureApplication.class, args);
+    }
+}
