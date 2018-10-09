0267c3b5efc287b9efd30b7843f5d094e7afd767
a5vTech <jesper2604@gmail.com>
2018-10-09 20:03:17 +0200

    Login

--- src/main/java/com/adventure/User.java
+++ src/main/java/com/adventure/User.java
@@ -1,4 +1,41 @@
 package com.adventure;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class User {
+
+    private String username;
+    private String password;
+    private int accessLevel;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAccessLevel() {
+        return accessLevel;
+    }
+
+    public void setAccessLevel(int accessLevel) {
+        this.accessLevel = accessLevel;
+    }
+
+
+
 }
