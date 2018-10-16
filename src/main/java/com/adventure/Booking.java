10c5f259dd5ce299d3eb46dc26c6ec95c5890b03
DESKTOP-1HIU046\Leader <jesp688a@stud.kea.dk>
2018-10-16 11:44:53 +0200

    Getters and setters

--- src/main/java/com/adventure/Booking.java
+++ src/main/java/com/adventure/Booking.java
@@ -258,6 +258,43 @@ public class Booking {
         this.price = price;
     }
 
+
+    public void setPeopleCount(int peopleCount) {
+        this.peopleCount = peopleCount;
+    }
+
+    public String getCustomer_name() {
+        return customer_name;
+    }
+
+    public void setCustomer_name(String customer_name) {
+        this.customer_name = customer_name;
+    }
+
+    public String getCustomer_phone() {
+        return customer_phone;
+    }
+
+    public void setCustomer_phone(String customer_phone) {
+        this.customer_phone = customer_phone;
+    }
+
+    public String getCustomer_email() {
+        return customer_email;
+    }
+
+    public void setCustomer_email(String customer_email) {
+        this.customer_email = customer_email;
+    }
+
+    public String getCustomer_company() {
+        return customer_company;
+    }
+
+    public void setCustomer_company(String customer_company) {
+        this.customer_company = customer_company;
+    }
+
     @Override
     public String toString() {
 //        return "Booking{" +
